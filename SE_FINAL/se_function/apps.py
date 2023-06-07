3a208894aa47cfcd8c88e3509a144f1a7bedff79
PinRay22 <102412799+PinRay22@users.noreply.github.com>
2023-06-08 02:12:47 +0800

    Delete SE_FINAL directory

--- SE_FINAL/se_function/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SeFunctionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'se_function'
--- SE_FINAL/se_function/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import *
-# Create your views here.
-
-def index_view(request):
-    record = RevenueRecord.objects.all()
-    revenue_amount_list = [entry.revenue_amount for entry in record]
-    print(revenue_amount_list)
-    return render(request, 'index.html', locals())
